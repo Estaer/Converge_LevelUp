5eca6aa8ad4922a36e98eeb9f4c64095bf1c6ef9
Estaer <estaerbaekar@gmail.com>
2018-12-04 18:38:09 +0300

    feat: implement share functionality
     - create a Button to share user profile.
    
    [Finishes #156008359]

--- app/src/main/java/com/example/esthernamanda/converge_levelup/DeveloperDetails.java
+++ app/src/main/java/com/example/esthernamanda/converge_levelup/DeveloperDetails.java
@@ -3,6 +3,8 @@ package com.example.esthernamanda.converge_levelup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,20 +14,25 @@ import adapter.GithubUsersAdapter;
 
 public class DeveloperDetails extends AppCompatActivity {
 
+    String username = "";
+    String avatar_url = "";
+    String organisation_url = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer_details);
 
+        Intent intent = getIntent();
+         username = intent.getStringExtra(GithubUsersAdapter.USERNAME);
+         avatar_url = intent.getStringExtra(GithubUsersAdapter.AVATAR_URL);
+         organisation_url = intent.getStringExtra(GithubUsersAdapter.ORGANISATION_URL);
         setData();
 
     }
 
     public void  setData (){
-        Intent intent = getIntent();
-        String username = intent.getStringExtra(GithubUsersAdapter.USERNAME);
-        String avatar_url = intent.getStringExtra(GithubUsersAdapter.AVATAR_URL);
-        String organisation_url = intent.getStringExtra(GithubUsersAdapter.ORGANISATION_URL);
+
 
         TextView username_txt = findViewById(R.id.username_details_txt);
         username_txt.setText(username);
@@ -39,5 +46,26 @@ public class DeveloperDetails extends AppCompatActivity {
                 load(avatar_url).
                 into(profile_img);
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String message = String.format("Check out this awesome developer @%s, %s", username, organisation_url);
+        int id = item.getItemId();
+        if (id == R.id.mnShare){
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message);
+            shareIntent.setType("text/plain");
+            startActivity(shareIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
